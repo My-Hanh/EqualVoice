1d3f63c99a91e5e43ee230a03e797b28c3e4b75d
My-Hanh <dangmyh1@students.zhaw.ch>
2024-11-01 19:58:44 +0100

    Add code for Streamlit

--- app.py
+++ app.py
@@ -1,32 +1,27 @@
 from openai import OpenAI
 import streamlit as st
 
-# with st.sidebar:
-#     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-#     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-#     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-#     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-openai_api_key = "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"
 
-st.title("💬 Chatbot")
-st.caption("🚀 A Streamlit chatbot powered by OpenAI")
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
 
-if prompt := st.chat_input():
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
+st.title("Text Bias Detector")
 
-    client = OpenAI(api_key=openai_api_key)
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = response.choices[0].message.content
-    st.session_state.messages.append({"role": "assistant", "content": msg})
-    st.chat_message("assistant").write(msg)
+import streamlit as st
 
+st.text_area(
+    label="",
+    value="",
+    height= 500,  # Let CSS control height
+    max_chars=None,
+    key=None,
+    help="Enter your text here.",
+    on_change=None,
+    args=None,
+    kwargs=None,
+    placeholder="Type here...",
+    disabled=False,
+    label_visibility="visible"
+)
+
+st.button("Click me")
